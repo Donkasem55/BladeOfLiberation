19a2e1c4a44a8bbc0a16cebbd2dc670c8cba7f52
Raine <92718237+Donkasem55@users.noreply.github.com>
2026-07-13 19:39:34 +0800

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize2.h"
-
-#include <glad/gl.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <map>
@@ -12,6 +9,8 @@
 #include <sstream>
 #include <vector>
 #include <fstream>
+
+#include <iostream>
 
 const int tilewidth = 384;
 const int tileheight = 192;
@@ -43,49 +42,62 @@ std::vector<std::string> split(std::string input, char delimiter) {
 std::vector<std::vector<int>> readmap(const char* filename) {
 	std::ifstream readfile(filename);
 	std::string line;
-	std::vector<std::vector<std::string>> output;
+	std::vector<std::vector<int>> output;
 	while (std::getline(readfile, line)) {
 		std::vector<int> tmp;
 		for (const auto& val : split(line, ' ')) {
 			tmp.push_back(std::stoi(val));
+			std::cout << val << " ";
 		}
 		output.push_back(tmp);
+		std::cout << std::endl;
 	}
 	return output;
 }
 
 unsigned int loadTexture(const char* texture, int tileid) {
-	unsigned int imgwidth, imgheight, channels;
+	std::cout << tileid << std::endl;
+	int imgwidth, imgheight, channels;
+	unsigned char* textTMP = nullptr;
+	unsigned char* text = nullptr;
+	std::vector<unsigned char> txt(32 * 32 * 4);
 	if (tileid == -1) {
-		unsigned char* textTMP = stbi_load("empty.png", &imgwidth, &imgheight, &channels, 4);
-		unsigned char* text = textTMP;
-	} else {
-		unsigned char* textTMP = stbi_load(texture, &imgwidth, &imgheight, &channels, 4);
-		unsigned char tile[32 * 32 * 4];
+		textTMP = stbi_load("empty.png", &imgwidth, &imgheight, &channels, 4);
+		text = textTMP;
+		//tileid = 12;
+	}
+	{
+		textTMP = stbi_load(texture, &imgwidth, &imgheight, &channels, 4);
+		if (!textTMP) {
+			std::cout << stbi_failure_reason() << std::endl;
+		}
 		int tx = tileid % 8;
 		int ty = tileid / 8;
 
 		for (int y = 0; y < 32; y++) {
 			memcpy(
-				tile + y * 32 * 4,
-				data + ((ty * 32 + y) * width + tx * 32) * 4,
+				txt.data() + y * 32 * 4,
+				textTMP + ((ty * 32 + y) * imgwidth + tx * 32) * 4,
 				32 * 4
 			);
 		}
-		unsigned char text* = &tile;
+		text = txt.data();
+		
 		imgwidth = 32;
 		imgheight = 32;
 	}
 
-	unsigned int textID = glGenTextures(1);
+	unsigned int textID;
+	glGenTextures(1, &textID);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glBindTexture(GL_TEXTURE_2D, textID);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+	//std::cout << text[0];   for some reason it just freezes if you uncomment this line
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgwidth, imgheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, text);
 	stbi_image_free(textTMP);
 	return textID;
@@ -93,7 +105,7 @@ unsigned int loadTexture(const char* texture, int tileid) {
 
 void drawImg(int x, int y, int w, int h, unsigned int textureID) {
 	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, textureID);
 
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0);
@@ -113,8 +125,10 @@ int main(int argc, char* argv[]) {
 	glfwInit();
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-	*GLFWwindow screen = glfwCreateWindow(800, 600, "Blade of Liberation", NULL, NULL);
+	GLFWwindow* screen = glfwCreateWindow(800, 600, "Blade of Liberation", NULL, NULL);
 	glfwMakeContextCurrent(screen);
+	gladLoadGL();
+
 	glClearColor(0.2, 0.2, 0.2, 1.0);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -122,12 +136,13 @@ int main(int argc, char* argv[]) {
 	glDisable(GL_SCISSOR_TEST);
 	glDisable(GL_CULL_FACE);
 
-	int mvmnt[4] = [GLFW_KEY_E, GLFW_KEY_D, GLFW_KEY_S, GLFW_KEY_F];
+	int mvmnt[4] = {GLFW_KEY_E, GLFW_KEY_D, GLFW_KEY_S, GLFW_KEY_F};
 
 	std::vector<std::vector<int>> floormap = readmap("floor.txt");
 	std::vector<std::vector<int>> wallmap = readmap("wall.txt");
 	
 	std::map<int, unsigned int> textID;
+	unsigned int smth = loadTexture("assets/tiles.png", 1);
 
 	while (!glfwWindowShouldClose(screen)) {
 		glfwGetWindowSize(screen, &width, &height);
@@ -154,16 +169,30 @@ int main(int argc, char* argv[]) {
 		}
 
 		glClear(GL_COLOR_BUFFER_BIT);
-
 		for (int i=0; i<floormap.size(); i++) {
 			for (int j=0; j<floormap[i].size(); j++) {
-				if (textID.find(floormap[i][j]) != textID.end()) {
-					textID[floormap[i][j]] = loadTexture("assets/tiles.png", floormap[i][j])
+				if (textID.find(floormap[i][j]) == textID.end()) {
+					textID[floormap[i][j]] = loadTexture("assets/tiles.png", floormap[i][j]);
 				}
 				getscrxy(j, i, &tmpx, &tmpy);
-				drawImg(x-px+(width/2),y+py-48, 384, 384, textID[floormap[i][j]])
+				drawImg(tmpx-px+(width/2), tmpy+py, 384, 384, textID[floormap[i][j]]);
+
 			}
 		}
+		for (int i=0; i<wallmap.size(); i++) {
+			for (int j=0; j<wallmap[i].size(); j++) {
+				if (textID.find(wallmap[i][j]) == textID.end()) {
+					textID[wallmap[i][j]] = loadTexture("assets/tiles.png", wallmap[i][j]);
+				}
+				getscrxy(j, i, &tmpx, &tmpy);
+				drawImg(tmpx-px+(width/2), tmpy+py-48, 384, 384, textID[wallmap[i][j]]);
+			}
+		}
+		drawImg(0, 0, 32, 32, smth);
+		
+		glfwSwapBuffers(screen);
+		glfwPollEvents();
 	}
+	glfwTerminate();
 }
 
